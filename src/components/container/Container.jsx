382ca838a1fb2ea0708f0f4442bd8c63dee8bf26
Rajeshwar Singh <132039556+Raj4478@users.noreply.github.com>
2024-09-19 00:06:39 +0530

    Delete src directory

--- src/components/container/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Container({children}) {
-  return (
-    <div className='w-full max-w-8xl mx-auto px-4 ju text-center grid-flow-col '>{children}</div>
-  )
-}
-
-export default Container
